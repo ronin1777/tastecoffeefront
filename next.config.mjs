6ead250f22fb03485888ad900f1d1a50d1880fc5
comiser <comiser@comiser.comiser>
2024-10-23 14:54:49 +0330

    add url to nextjs.conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "127.0.0.1", "tastecoffee.liara.run"],
+    domains: [
+      "localhost",
+      "127.0.0.1",
+      "tastecoffee.liara.run",
+      "tastecoffee.darkube.app",
+    ],
   },
 };
 
