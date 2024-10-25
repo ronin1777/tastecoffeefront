30bde2e62aab2325e65816ab23a4ef0b86f1cc72
comiser <comiser@comiser.comiser>
2024-10-25 17:13:57 +0330

    fix cookie

--- app/api/auth/set-cookies/route.js
+++ app/api/auth/set-cookies/route.js
@@ -1,26 +1,25 @@
-import { NextResponse } from 'next/server';
-
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const { access, refresh } = await request.json();
 
   const response = NextResponse.json({ success: true });
 
-response.cookies.set('access_token', access, {
-  httpOnly: false,
-  secure: true,
-  maxAge: 60 * 60,
-  path: '/',
-  sameSite: 'Lax',
-});
+  response.cookies.set("access_token", access, {
+    httpOnly: true,
+    secure: true,
+    maxAge: 60 * 60,
+    path: "/",
+    sameSite: "Lax",
+  });
 
-response.cookies.set('refresh_token', refresh, {
-  httpOnly: false,
-  secure: true,
-  maxAge: 60 * 60 * 24 * 7,
-  path: '/',
-  sameSite: 'Lax',
-});
+  response.cookies.set("refresh_token", refresh, {
+    httpOnly: true,
+    secure: true,
+    maxAge: 60 * 60 * 24 * 7,
+    path: "/",
+    sameSite: "Lax",
+  });
 
   return response;
 }
