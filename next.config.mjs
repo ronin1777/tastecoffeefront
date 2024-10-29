ac447c78bdea5ea09e5a21dcd564c73ae0595bf9
comiser <comiser@comiser.comiser>
2024-10-29 20:28:07 +0330

    add NEXT_PUBLIC_API_URL

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
     remotePatterns: [
       {
