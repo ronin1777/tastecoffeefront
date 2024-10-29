0123dfa9c13843af30b7c956da1883e98e42bc91
comiser <comiser@comiser.comiser>
2024-10-29 20:03:45 +0330

    fixed env url

--- services/config.js
+++ services/config.js
@@ -1,4 +1,4 @@
-// const apiUrl = "https://tastecoffee.darkube.app";
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiUrl = "https://tastecoffee.darkube.app";
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default apiUrl;
